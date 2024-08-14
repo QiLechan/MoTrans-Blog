ef187929c2ab0de37fb2cfcfcf76d016c3d83f67
七楽 <88995341+QiLechan@users.noreply.github.com>
2024-08-14 14:50:03 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://astronaut.github.io',
-	base: 'MoTrans-Blog',
+	site: 'https://motranstest.nanaraku.cn',
 	integrations: [mdx(), sitemap()],
 });
